a2200c5207a466d64f7cc827d22dd16218c13ac4
Joseph DALY <joseph300905@gmail.com>
2025-05-28 21:05:07 +0200

    quick usage of a brain

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "ai.h"
 #include "game.h"
 
@@ -32,6 +34,35 @@ int
 main()
 {
   ai_brain_t brain;
+  chess_game_t game;
+  float inputs[768];
+  size_t i, highest_score = 0, src_index, dst_index;
+  uint8_t src_x, src_y, dst_x, dst_y;
+
+  for (i = 0; i < 768; i++) {
+    inputs[i] = 0.0f;
+  }
+
   init_chess_brain(&brain);
+  chess_init(&game);
+  encode_chess_board(game.board, inputs);
+  ai_brain_forward(&brain, inputs);
+
+  for (i = 0; i < 4096; i++) {
+    if (brain.layers[2].outputs[i] > brain.layers[2].outputs[highest_score]) {
+      highest_score = i;
+    }
+  }
+
+  src_index = highest_score / 64;
+  dst_index = highest_score % 64;
+
+  src_x = src_index / 8;
+  src_y = src_index % 8;
+  dst_x = dst_index / 8;
+  dst_y = dst_index % 8;
+
+  printf("highest score: %ld (x: %d, y: %d to x: %d, y: %d)\n", highest_score, src_x, src_y, dst_x, dst_y);
+
   ai_brain_free(&brain);
 }
