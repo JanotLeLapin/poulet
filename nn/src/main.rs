2be9f69f60c6420cc328f5b9036f20d707afc83a
Joseph DALY <joseph300905@gmail.com>
2025-11-07 23:38:40 +0100

    make encode_board return a Vec<f32>

--- nn/src/main.rs
+++ nn/src/main.rs
@@ -5,7 +5,9 @@ use crate::model::predict_move;
 
 mod model;
 
-fn encode_board(board: &Board, neurons: &mut Vec<f32>) {
+fn encode_board(board: &Board) -> Vec<f32> {
+    let mut neurons = Vec::with_capacity(768);
+
     let piece_iter = vec![Color::White, Color::Black].into_iter().flat_map(|c| {
         vec![
             PieceType::Pawn,
@@ -19,18 +21,19 @@ fn encode_board(board: &Board, neurons: &mut Vec<f32>) {
         .map(move |t| Piece::new(c, t))
     });
 
-    for (i, piece) in piece_iter.enumerate() {
-        for x in 0..8 {
-            for y in 0..8 {
-                let neuron_idx = i * 64 + (y * 8 + x);
-                neurons[neuron_idx] = if board.get_square(x as u8, y as u8) == Some(piece) {
+    for piece in piece_iter {
+        for y in 0..8 {
+            for x in 0..8 {
+                neurons.push(if board.get_square(x as u8, y as u8) == Some(piece) {
                     1.0
                 } else {
                     0.0
-                };
+                });
             }
         }
     }
+
+    neurons
 }
 
 fn main() {
@@ -39,9 +42,10 @@ fn main() {
     let device = Default::default();
     let model = model::ModelConfig::new().init::<MyBackend>(&device);
 
-    let mut neurons = (0..768).map(|_| 0.0).collect();
+    let mut neurons = Vec::with_capacity(768);
     let mut game = Game::default();
-    encode_board(&game.board, &mut neurons);
+    neurons.extend(encode_board(&game.board));
+
     let data = TensorData::new(neurons, [1, 768]);
     let tensor: Tensor<MyBackend, 2> = Tensor::from_floats(data, &device);
 
