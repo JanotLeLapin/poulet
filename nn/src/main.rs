4b2ef616be84b0522d6e49697bcc60509469bd80
Joseph DALY <joseph300905@gmail.com>
2025-11-07 19:22:39 +0100

    encode chess board onto neurons

--- nn/src/main.rs
+++ nn/src/main.rs
@@ -1,6 +1,35 @@
-use burn::backend::Wgpu;
+use burn::{Tensor, backend::Wgpu, tensor::TensorData};
+use poulet_chess::{Board, Color, Game, Piece, PieceType};
 
 mod model;
+
+fn encode_board(board: &Board, neurons: &mut Vec<f32>) {
+    let piece_iter = vec![Color::White, Color::Black].into_iter().flat_map(|c| {
+        vec![
+            PieceType::Pawn,
+            PieceType::Bishop,
+            PieceType::Knight,
+            PieceType::Rook,
+            PieceType::Queen,
+            PieceType::King,
+        ]
+        .into_iter()
+        .map(move |t| Piece::new(c, t))
+    });
+
+    for (i, piece) in piece_iter.enumerate() {
+        for x in 0..8 {
+            for y in 0..8 {
+                let neuron_idx = i * 64 + (y * 8 + x);
+                neurons[neuron_idx] = if board.get_square(x as u8, y as u8) == Some(piece) {
+                    1.0
+                } else {
+                    0.0
+                };
+            }
+        }
+    }
+}
 
 fn main() {
     type MyBackend = Wgpu<f32, i32>;
