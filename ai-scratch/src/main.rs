25fda2623d423496dbd25a9f20bf57b994920462
Joseph DALY <joseph300905@gmail.com>
2025-11-08 13:54:52 +0100

    move nn to ai-scratch

--- ai-scratch/src/main.rs
+++ ai-scratch/src/main.rs
@@ -1,10 +1,10 @@
-use poulet_nn::{State, encode_board};
+use poulet_ai_scratch::{Player, State, encode_board};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let state = State::new().await;
 
-    let player = poulet_nn::Player::new(&state)?;
+    let player = Player::new(&state)?;
 
     let mut input_data: Vec<f32> = (0..768).map(|_| 0.0 as f32).collect();
     encode_board(&poulet_chess::Game::default().board, &mut input_data);
