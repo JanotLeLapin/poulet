50f8b1b2266b24ed4b5f6975122449490b8bb1cc
Joseph DALY <joseph300905@gmail.com>
2025-11-09 16:03:14 +0100

    [scratch] switch to builder clap api

--- ai-scratch/src/main.rs
+++ ai-scratch/src/main.rs
@@ -3,58 +3,74 @@ use poulet_ai_scratch::{
     model::{Player, State},
 };
 
-use clap::Parser;
+use clap::{Command, arg, value_parser};
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Generation where the algorithm left off
-    #[arg(short, long)]
-    generation: u32,
-
-    /// Model save interval
-    #[arg(short, long, default_value_t = 5)]
-    save_interval: u32,
-
-    /// Number of individuals in the elite
-    #[arg(short, long, default_value_t = 8)]
-    elite_size: usize,
+fn cli() -> Command {
+    Command::new("poulet")
+        .about("A toy generic algorithm that learns to play chess")
+        .subcommand_required(true)
+        .subcommands([Command::new("train")
+            .arg(
+                arg!(<GEN> "Generation where the algorithm left off")
+                    .required(true)
+                    .value_parser(value_parser!(usize)),
+            )
+            .arg(
+                arg!(--interval [INTERVAL] "Generation save interval")
+                    .value_parser(value_parser!(usize)),
+            )
+            .arg(
+                arg!(--elite [ELITE] "Number of elite individuals for each generation")
+                    .value_parser(value_parser!(usize)),
+            )])
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let args = Args::parse();
-
     let state = State::new().await;
 
-    let mut g = args.generation;
-    let mut generation;
-    let mut elite;
+    let m = cli().get_matches();
+    match m.subcommand() {
+        Some(("train", sub)) => {
+            let mut g: usize = *sub.get_one::<usize>("GEN").unwrap();
+            let save_interval: usize = *sub.get_one("INTERVAL").unwrap_or(&5);
+            let elite_size: usize = *sub.get_one("ELITE").unwrap_or(&8);
 
-    if g == 0 {
-        generation = Generation::init(&state);
-        generation.play();
-        elite = generation.get_elite(args.elite_size);
-    } else {
-        elite = (0..args.elite_size)
-            .map(|i| {
-                Player::load(&state, &format!("./ai-scratch/models/model-{g}-{i}.params")).unwrap()
-            })
-            .collect();
-    }
+            let mut generation;
+            let mut elite;
 
-    loop {
-        println!("--- GENERATION {g} ---");
-        let mut generation = Generation::populate(&state, elite);
-        generation.play();
-        elite = generation.get_elite(args.elite_size);
-        g += 1;
+            if g == 0 {
+                generation = Generation::init(&state);
+                generation.play();
+                elite = generation.get_elite(elite_size);
+            } else {
+                elite = (0..elite_size)
+                    .map(|i| {
+                        Player::load(&state, &format!("./ai-scratch/models/model-{g}-{i}.params"))
+                            .unwrap()
+                    })
+                    .collect();
+            }
 
-        if g % args.save_interval == 0 {
-            elite.iter().enumerate().for_each(|(i, p)| {
-                p.save(&format!("./ai-scratch/models/model-{g}-{i}.params"))
-                    .unwrap()
-            });
+            loop {
+                println!("--- GENERATION {g} ---");
+                let mut generation = Generation::populate(&state, elite);
+                generation.play();
+                elite = generation.get_elite(elite_size);
+                g += 1;
+
+                if g % save_interval == 0 {
+                    elite.iter().enumerate().for_each(|(i, p)| {
+                        p.save(&format!("./ai-scratch/models/model-{g}-{i}.params"))
+                            .unwrap()
+                    });
+                }
+            }
+        }
+        _ => {
+            cli().print_help()?;
         }
     }
+
+    Ok(())
 }
